04e08f3df18a31b850c5a85543fadb744729db8b
Abhishek Nilange <abhishek.nilange25@gmail.com>
2020-05-07 11:23:47 +0530

    refactor: code

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
+import os
 from flask import Flask
+from flask import render_template
+import socket
+import random
+
 app = Flask(__name__)
 
+color_codes = {
+    "red": "#e74c3c",
+    "green": "#16a085",
+    "blue": "#2980b9",
+    "blue2": "#30336b",
+    "pink": "#be2edd",
+    "darkblue": "#130f40"
+}
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue2","darkblue","pink"])
 
-@app.route('/status')
-def status():
-    return "Active"
+@app.route("/")
+def main():
+    print(color)
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+@app.route('/color/<new_color>')
+def new_color(new_color):
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
+
+@app.route('/read_file')
+def read_file():
+    f = open("/data/testfile.txt")
+    contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080")
